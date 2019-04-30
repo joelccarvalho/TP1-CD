fd31d5128011ba8faad52874b93c6409512ff9b8
Joel Carvalho <joelccarv@gmail.com>
2019-04-30 21:40:12 +0100

    Update
    Sender progress

--- Receiver/Receiver.cs
+++ Receiver/Receiver.cs
@@ -1,49 +1,60 @@
-using System;
+﻿using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Collections.Generic;
 
-namespace Receiver
+namespace UDp_Multicast_Receiver
 {
-
     class Receiver
     {
-
         public static int SEQ = 20191;
         public static int SYN = 1;
         public static int ACK = 0;
         public static int MSS = 8192;
         public static int SEGM = 1;
+        public static float NUM_SEGM = 5; // Default
         public static int temp = 0;
+        public static bool retras = false;
+        public static int tryRetras = 5; // Number retransmissions
 
         static void Main(string[] args)
         {
-            string optn         = getMenu();
-            int ctn             = 2;
-            Socket s            = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            string optn = GetMenu();
+            int ctn = 2;
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             IPAddress broadcast = IPAddress.Parse("127.0.0.1");
-            UdpClient listener  = new UdpClient(11000);
-            IPEndPoint groupEP  = new IPEndPoint(IPAddress.Any, 11000);
-            
+            UdpClient listener = new UdpClient(11000);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 11000);
+
             /** 
               * CTN = 0: Close connection  
               * CTN = 1: Reset and reconection with new file
-              * CTN = 2: First connection  
+              * CTN = 2: Connection  
             **/
 
             switch (optn)
             {
                 case "1":
-                    while (SEGM < 6 || ctn > 0)
+                    while (SEGM < (NUM_SEGM-1) || ctn > 0)
                     {
-                        if(ctn == 1) {
+                        if (ctn == 1)
+                        {
                             Console.Clear();
-                            resetConnection();
+                            ResetConnection();
                         }
 
-                        getConnection(listener, groupEP);
-                        ctn = sendResponse(s, broadcast);
+                        if(ctn == 0 && tryRetras < 0) {
+                            ResetConnection();
+                        }
+                        else if(tryRetras == 0) {
+                            break;
+                        }
+
+                        ctn = GetConnection(listener, groupEP);
+                        if(ctn != 0)
+                        {
+                            ctn = SendResponse(s, broadcast);
+                        }
                     }
                     break;
                 case "0":
@@ -58,13 +69,13 @@ namespace Receiver
             Console.ReadLine();
         }
 
-        static string getMenu()
+        static string GetMenu()
         {
             string optn;
 
             Console.WriteLine("*************************");
             Console.WriteLine("MENU");
-            Console.WriteLine("1 - Receber dados liga��o UDP");
+            Console.WriteLine("1 - Receber dados ligação UDP");
             Console.WriteLine("0 - Sair");
             Console.WriteLine("*************************");
             optn = Console.ReadLine();
@@ -72,11 +83,13 @@ namespace Receiver
             return optn;
         }
 
-        static void getConnection(UdpClient listener, IPEndPoint groupEP)
+        static int GetConnection(UdpClient listener, IPEndPoint groupEP)
         {
+            Console.WriteLine("Aguardar dados...");
+            listener.Client.ReceiveTimeout = 5000;
+
             try
             {
-                Console.WriteLine("Aguardar dados...");
                 byte[] bytes = listener.Receive(ref groupEP);
                 string content = Encoding.ASCII.GetString(bytes);
                 string[] info = content.Split('_');
@@ -85,9 +98,9 @@ namespace Receiver
                 {
                     MSS = int.Parse(info[3]);
                 }
-                
-                SYN  = int.Parse(info[0]);
-                ACK  = int.Parse(info[1]) + 1;
+
+                SYN = int.Parse(info[0]);
+                ACK = int.Parse(info[1]) + 1;
                 SEGM = int.Parse(info[4]) + 1;
 
                 if (SYN == 0 && temp >= 1)
@@ -95,7 +108,10 @@ namespace Receiver
                     SEQ = int.Parse(info[2]);
                     ACK = int.Parse(info[1]) + MSS;
                 }
-                
+
+                // Update num segments
+                NUM_SEGM = int.Parse(info[5]);
+
                 // Show status
                 Console.WriteLine("***********");
                 Console.WriteLine("SEGMENTO: {0}", SEGM);
@@ -103,11 +119,16 @@ namespace Receiver
                 Console.WriteLine("SYN: {0}", SYN);
                 Console.WriteLine("SEQ: {0}", SEQ);
                 Console.WriteLine("ACK: {0}", ACK);
-                Console.WriteLine("***********");    
+                Console.WriteLine("***********");
+
+                return 2;
             }
             catch (SocketException e)
             {
-                Console.WriteLine(e);
+                retras = true;
+                tryRetras--;
+                Console.WriteLine("Erro, na receção da resposta! Tentativas disponíveis: {0}", tryRetras);
+                return 0;
             }
             // finally
             // {
@@ -115,7 +136,7 @@ namespace Receiver
             // }
         }
 
-        static int sendResponse(Socket s, IPAddress broadcast)
+        static int SendResponse(Socket s, IPAddress broadcast)
         {
             string content = SYN.ToString() + '_' + SEQ.ToString() + '_' + ACK.ToString() + '_' + MSS.ToString() + '_' + SEGM.ToString();
 
@@ -125,7 +146,6 @@ namespace Receiver
                 IPEndPoint ep = new IPEndPoint(broadcast, 11001);
 
                 s.SendTo(sendbuf, ep);
-                //s.Send(sendbuf);
                 Console.WriteLine("Mensagem enviada!");
 
                 temp++;
@@ -135,7 +155,8 @@ namespace Receiver
                 Console.WriteLine(e);
             }
 
-            if(SEGM >= 6) {
+            if (SEGM >= (NUM_SEGM-1))
+            {
                 Console.WriteLine("Deseja continuar à escuta de outros ficheiros?");
                 Console.WriteLine("1 - Sim");
                 Console.WriteLine("0 - Não");
@@ -147,13 +168,16 @@ namespace Receiver
             return 2;
         }
 
-        static void resetConnection(){
-            SEQ = 20191;
-            SYN = 1;
-            ACK = 0;
-            MSS = 8192;
-            SEGM = 1;
-            temp = 0;
+        static void ResetConnection()
+        {
+            SEQ         = 20191;
+            SYN         = 1;
+            ACK         = 0;
+            MSS         = 8192;
+            SEGM        = 1;
+            temp        = 0;
+            retras      = false;
+            tryRetras   = 5;
         }
-    }
+    }    
 }
--- Sender/Sender.cs
+++ Sender/Sender.cs
@@ -1,15 +1,13 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Drawing;
-using System.Collections;
-using System.Collections.Generic;
 
-namespace Sender
+namespace UDP_Multicast_Sender
 {
-    public class Sender
+    class Sender
     {
         //public static IDictionary<string, int> configs = new Dictionary<string, int>();
         //public static int[] configs = new int[2];
@@ -20,34 +18,55 @@ namespace Sender
         public static int SEGM = 1;
         public static int temp = 0;
         public static int SIZE_FILE = 0;
+        public static byte[] BUFFER_FILE;
+        public static float NUM_SEGM = 4; // Default
         public static UdpClient listener = new UdpClient(11001);
+        public static bool retras = false;
+        public static int tryRetras = 5; // Number retransmissions
 
         static void Main(string[] args)
         {
-            string optn         = getMenu();
-            int ctn             = 2;
-            Socket s            = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            string optn = GetMenu();
+            int ctn = 2;
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             IPAddress broadcast = IPAddress.Parse("127.0.0.1");
-            IPEndPoint groupEP  = new IPEndPoint(IPAddress.Any, 11000);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 11000);
 
             /** 
               * CTN = 0: Close connection  
               * CTN = 1: Reset and reconection with new file
-              * CTN = 2: First connection  
+              * CTN = 2: Connection  
             **/
 
             switch (optn)
             {
                 case "1":
-                    while (SEGM < 7 || ctn > 0)
+                    while (SEGM < NUM_SEGM || ctn > 0)
                     {
-                        if(ctn == 1) {
+                        // Reset connection
+                        if (ctn == 1)
+                        {
                             Console.Clear();
-                            resetConnection();  
+                            ResetConnection();
+                        }
+                        
+                        if(ctn == 0 && tryRetras < 0) {
+                            ResetConnection();
+                        }
+                        else if(tryRetras == 0) {
+                            break; // Timeout
                         }
 
-                        getConnection(s, broadcast);
-                        ctn = receiveResponse(groupEP);
+                        // Open connection
+                        string path = GetConnection(s, broadcast);
+                        // Receive data
+                        ctn         = ReceiveResponse(groupEP);
+
+                        // Send file
+                        if(ctn == 2)
+                        {
+                            SendFile(s, broadcast, path, groupEP);
+                        }
                     }
                     break;
                 case "0":
@@ -61,7 +80,8 @@ namespace Sender
             Console.WriteLine("Pressione ENTER para fechar!");
             Console.ReadLine();
         }
-        static string getMenu()
+
+        static string GetMenu()
         {
             string optn;
 
@@ -75,105 +95,160 @@ namespace Sender
             return optn;
         }
 
-        static void getConnection(Socket s, IPAddress broadcast)
+        static string GetConnection(Socket s, IPAddress broadcast)
         {
             string path = null;
 
             // Only first time
-            if(SYN == 1)
+            if (SYN == 1)
             {
                 Console.WriteLine("Indique o caminho do ficheiro" + @" Exemplo:(C:\Users\utilizador\Desktop\nomeFicheiro.extensao)");
-                path = @"/Users/joelcarvalho/Desktop/teste.txt";
+                path = @"/Users/joelcarvalho/Desktop/horario.png";
                 //path = Console.ReadLine();
-
-                fileRequest(path);
+                FileRequest(path);
             }
 
-            //configs.Add("SEQ", 109876);
-            //configs.Add("SYN", 1);
-            //configs[0] = 109876;
-            //configs[1] = 1;
+            // Count number of required segments
+            CountSegm();
 
-            string content = SYN.ToString() + '_' + SEQ.ToString() + '_' + ACK.ToString() + '_' + MSS.ToString() + '_' + SEGM.ToString();
+            string content = SYN.ToString() + '_' + SEQ.ToString() + '_' + ACK.ToString() + '_' + MSS.ToString() + '_' + SEGM.ToString() + '_' + NUM_SEGM.ToString();
             try
-            {   
+            {
+                // Config send 
                 byte[] sendbuf = Encoding.ASCII.GetBytes(content);
-                IPEndPoint ep  = new IPEndPoint(broadcast, 11000);
+                IPEndPoint ep = new IPEndPoint(broadcast, 11000);
 
+                // Send content
                 s.SendTo(sendbuf, ep);
-                Console.WriteLine("Mensagem enviada!");                
+                Console.WriteLine("Mensagem enviada!");
             }
             catch (SocketException e)
             {
                 Console.WriteLine(e);
             }
+            return path;
         }
 
-        static int receiveResponse(IPEndPoint groupEP)
+        static void SendFile(Socket s, IPAddress broadcast, string path, IPEndPoint groupEP)
+        {
+            byte[] buffer = BUFFER_FILE;
+            byte[] split  = new byte[576];
+            int pos       = 575;
+            int cont      = 0; // Number iterations
+            int percent   = 0;
+
+            for (int i = 0; i < buffer.Length; i+= pos)
+            {
+                int comp = pos;
+                cont++;
+                Array.Copy(buffer, split, comp); // Copy position to array
+                string txt = SYN.ToString() + '_' + SEQ.ToString() + '_' + ACK.ToString() + '_' + MSS.ToString() + '_' + SEGM.ToString() + '_' + NUM_SEGM.ToString() + '_' + split;
+                try
+                {
+                    byte[] sendbuf = Encoding.ASCII.GetBytes(txt);
+                    IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+                    // Send content
+                    s.SendTo(sendbuf, ep);
+                    
+                    // Calculate percentage 
+                    percent = (((pos*cont) * 100)/SIZE_FILE < 100 ? ((pos*cont) * 100)/SIZE_FILE : 100);
+                    
+                    Console.WriteLine("A enviar ficheiro...");
+                    Console.WriteLine("Estado: {0}%", percent);
+
+                    // Waiting response
+                    if(percent < 100) {
+                        ReceiveResponse(groupEP); // Get ACK, SEQ...
+                    }
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }   
+        }
+
+        static int ReceiveResponse(IPEndPoint groupEP)
         {
             Console.WriteLine("Aguardar dados...");
-            byte[] bytes   = listener.Receive(ref groupEP);
-            string content = Encoding.ASCII.GetString(bytes);
-            string[] info  = content.Split('_');
+            listener.Client.ReceiveTimeout = 5000; // 5s timemout
 
-            // If ACK == (SEQ+1) and SYN_Receiver == 1 then connection granted
-            if(int.Parse(info[2]) == (SEQ + 1) && int.Parse(info[0]) == 1){
-                Console.WriteLine("***********");
-                Console.WriteLine("Ligação garantida!");
-                SYN = 0; MSS = 0;
-            }
-            else {
-                // SE NÃO FOR GARANTIDA, FAZER ALGO.....
-                Console.WriteLine("***********");
-            }
+            try {
+                byte[] bytes = listener.Receive(ref groupEP);
+                string content = Encoding.ASCII.GetString(bytes);
+                string[] info = content.Split('_');
+                
+                // If ACK == (SEQ+1) and SYN_Receiver == 1 then connection granted
+                if (int.Parse(info[2]) == (SEQ + 1) && int.Parse(info[0]) == 1)
+                {
+                    Console.WriteLine("***********");
+                    Console.WriteLine("Ligação garantida!");
+                    SYN = 0; MSS = 0;
+                }
+                else
+                {
+                    // SE NÃO FOR GARANTIDA, FAZER ALGO.....
+                    Console.WriteLine("***********");
+                }
 
-            if (MSS >= int.Parse(info[3]))
-            {
-                MSS = int.Parse(info[3]);
-            }
+                if (MSS >= int.Parse(info[3]))
+                {
+                    MSS = int.Parse(info[3]);
+                }
 
-            ACK  = int.Parse(info[1]) + 1;
-            SEQ  = int.Parse(info[2]);
-            SEGM = int.Parse(info[4]) + 1;
-
-            if (SYN == 0 && temp >= 1)
-            {
-                SEQ = int.Parse(info[2]) + MSS;
                 ACK = int.Parse(info[1]) + 1;
+                SEQ = int.Parse(info[2]);
+                SEGM = int.Parse(info[4]) + 1;
+
+                if (SYN == 0 && temp >= 1)
+                {
+                    SEQ = int.Parse(info[2]) + MSS;
+                    ACK = int.Parse(info[1]) + 1;
+                }
+
+                // Show status
+                Console.WriteLine("SEGMENTO: {0}", SEGM);
+                Console.WriteLine("MSS: {0}", MSS);
+                Console.WriteLine("SYN: {0}", SYN);
+                Console.WriteLine("SEQ: {0}", SEQ);
+                Console.WriteLine("ACK: {0}", ACK);
+                Console.WriteLine("***********");
+                temp++;
+
+                if (SEGM >= NUM_SEGM)
+                {
+                    Console.WriteLine("Deseja enviar outra ficheiro?");
+                    Console.WriteLine("1 - Sim");
+                    Console.WriteLine("0 - Não");
+                    string resp = Console.ReadLine();
+
+                    return (int.Parse(resp) == 1 ? 1 : 0);
+                }
+
+                return 2;
+
+            } catch(Exception e) {
+                retras = true;
+                tryRetras--;
+                Console.WriteLine("Erro, na receção da resposta! Tentativas disponíveis: {0}", tryRetras);
+                return 0;
             }
-
-            // Show status
-            Console.WriteLine("SEGMENTO: {0}", SEGM);
-            Console.WriteLine("MSS: {0}", MSS);
-            Console.WriteLine("SYN: {0}", SYN);
-            Console.WriteLine("SEQ: {0}", SEQ);
-            Console.WriteLine("ACK: {0}", ACK);
-            Console.WriteLine("***********");
-            temp++;
-
-            if(SEGM >= 7) {
-                Console.WriteLine("Deseja enviar outra ficheiro?");
-                Console.WriteLine("1 - Sim");
-                Console.WriteLine("0 - Não");
-                string resp = Console.ReadLine();
-
-                return (int.Parse(resp) == 1 ? 1 : 0);
-            }
-
-            return 2;
         }
 
-        static void fileRequest(String path) {
+        static void FileRequest(String path)
+        {
             byte[] sizeFile  = null;
             bool allowedSize = false;
             bool fileExists  = false;
 
             do
             {
+                // If path exists
                 if (File.Exists(path))
                 {
                     fileExists = true;
-                    sizeFile = File.ReadAllBytes(path);
+                    sizeFile   = File.ReadAllBytes(path); // Read all bytes
 
                     // Max 5MB
                     if (sizeFile.Length < 5242880)
@@ -193,21 +268,40 @@ namespace Sender
                 }
             } while (!fileExists || !allowedSize); // While path doesn't exists or size exceeds 5MB
 
-            Console.WriteLine("Ficheiro permitido");
-            Console.WriteLine("TAMANHO {0}", sizeFile.Length);
-            SIZE_FILE = sizeFile.Length;
+            Console.WriteLine("Ficheiro permitido!");
 
+            SIZE_FILE   = sizeFile.Length;
+            BUFFER_FILE = File.ReadAllBytes(path);
         }
 
-        static void resetConnection(){
-            SEQ = 109876;
-            SYN = 1;
-            ACK = 0;
-            MSS = 4096;
-            SEGM = 1;
-            temp = 0;
-            SIZE_FILE = 0;
+        static void ResetConnection()
+        {
+            SEQ         = 109876;
+            SYN         = 1;
+            ACK         = 0;
+            MSS         = 4096;
+            SEGM        = 1;
+            temp        = 0;
+            SIZE_FILE   = 0;
+            BUFFER_FILE = null;
+            retras      = false;
+            tryRetras   = 5;
+        }
+
+        static void CountSegm() {
+
+            NUM_SEGM = BUFFER_FILE.Length/576;
+
+            if((NUM_SEGM*576) < BUFFER_FILE.Length){
+                NUM_SEGM++; // Add 1 if necesary
+            }
+
+            // To finish
+            NUM_SEGM += 1;
+            // Add receiver segments
+            NUM_SEGM *= 2;
         }
     }
 }
+
 
